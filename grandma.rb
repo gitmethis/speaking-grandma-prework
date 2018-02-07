d3f730da03ecd953cbec22a446a954ed02a5ff09
Chase <chaseit12@gmail.com>
2018-02-07 17:23:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,12 +14,14 @@
 
 
 def speak_to_grandma(string)
+   if string == 'I LOVE YOU GRANDMA!'
+      return 'I LOVE YOU TOO PUMPKIN!'
+   end  
+  
    if string.upcase != string 
       return 'HUH?! SPEAK UP, SONNY!'
    elsif string.upcase == string
       return 'No, NOT SINCE 1938!'
-   elsif string == 'I LOVE YOU GRANDMA!'
-      return 'I LOVE YOU TOO PUMPKIN!'
    end
    
 end
