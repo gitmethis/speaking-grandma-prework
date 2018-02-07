57f804bfa3a6dbef21a275c8aefb44978cfb91fd
Chase <chaseit12@gmail.com>
2018-02-07 17:26:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(string)
   
    if string.upcase == string
       print 'yo'
-      return "No, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
    end
   
    if string.upcase != string 
